9beed894964b972fadfe2af450326e5c882b228d
gitlmos <50037779+gitlmos@users.noreply.github.com>
2019-10-04 13:50:59 +0700

    Add files via upload

---
+++ build/precache-manifest.64b9230d7f94b761479c0452f907d46b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140b41872f8c59c8041b7a50de7b6db9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a550c888b4a10040771f",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f14b8b7f6f58b11f660c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a550c888b4a10040771f",
+    "url": "/static/js/2.c83f4874.chunk.js"
+  },
+  {
+    "revision": "f14b8b7f6f58b11f660c",
+    "url": "/static/js/main.31f1a9f3.chunk.js"
+  },
+  {
+    "revision": "44a4e1cccb70fbd5cf02",
+    "url": "/static/js/runtime-main.4a1f9c0b.js"
+  }
+]);
